adc2cbb6aaea1dc3e450668514fe3249d9ae26a8
arafat-alim <arafat97alim@gmail.com>
2024-09-05 21:00:32 +0530

    🟢 Composite Button component: Successfully created.

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,3 +1,4 @@
+import { ButtonProps } from "@/types/image";
 import { Text, TouchableOpacity } from "react-native";
 
 const CustomButton = ({
@@ -9,11 +10,47 @@ const CustomButton = ({
   IconRight,
   className,
   ...props
-}) => {
+}: ButtonProps) => {
+  const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
+    switch (variant) {
+      case "secondary":
+        return "bg-grey-500";
+      case "danger":
+        return "bg-red-500";
+      case "success":
+        return "bg-green-500";
+      case "outline":
+        return "bg-transparent border-neutral-300 border-[0.5px]";
+      default:
+        return "bg-[#0286FF]";
+    }
+  };
+
+  const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
+    switch (variant) {
+      case "primary":
+        return "text-black";
+      case "secondary":
+        return "text-gray-100";
+      case "danger":
+        return "text-red-100";
+      case "success":
+        return "text-green-100";
+      default:
+        return "text-white";
+    }
+  };
+
   return (
-    <TouchableOpacity onPress={onPress}>
+    <TouchableOpacity
+      onPress={onPress}
+      className={`w-full rounded-full  flex flex-row items-center justify-center bg-red-300 shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+      {...props}
+    >
       {IconLeft && <IconLeft />}
-      <Text>{title}</Text>
+      <Text className={`font-bold text-lg ${getTextVariantStyle(textVariant)}`}>
+        {title}
+      </Text>
       {IconRight && <IconRight />}
     </TouchableOpacity>
   );
