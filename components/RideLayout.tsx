f4b82aed44829516882e6a2b97f88dff0b965972
arafat-alim <arafat97alim@gmail.com>
2024-09-13 01:25:15 +0530

    ✨ Composite component added, RideLayout and implemented the bottom sheet.

--- components/RideLayout.tsx
+++ components/RideLayout.tsx
@@ -1,12 +1,46 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants";
+import { router } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Map from "./Map";
+import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import { useRef } from "react";
 
-const RideLayout = ({ children }: { children: React.ReactNode }) => {
+const RideLayout = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) => {
+  const bottomSheetRef = useRef<BottomSheet>();
   return (
-    <View>
-      <Text>TOP OF THE LAYOUT</Text>
-      {children}
-      <Text>BOTTOM OF THE LAYOUT</Text>
-    </View>
+    <GestureHandlerRootView>
+      <View className="flex-1 bg-white">
+        <View className="flex flex-col h-screen bg-blue-500">
+          <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
+            <TouchableOpacity onPress={() => router.back()}>
+              <View className="w-10 h-10 bg-white rounded-full items-center justify-center">
+                <Image
+                  source={icons.backArrow}
+                  className="h-6 w-6"
+                  resizeMode="contain"
+                />
+              </View>
+            </TouchableOpacity>
+            <Text className="text-xl font-JakartaSemiBold ml-5">
+              {title || "Go Back"}
+            </Text>
+          </View>
+          <Map />
+        </View>
+        <BottomSheet ref={bottomSheetRef} snapPoints={["40%", "85%"]} index={0}>
+          <BottomSheetScrollView style={{ flex: 1, padding: 20 }}>
+            {children}
+          </BottomSheetScrollView>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
   );
 };
 
