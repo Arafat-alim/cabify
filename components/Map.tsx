ee6b50b40a38e2dfc48ae714b78e41aedb4a04fe
arafat-alim <arafat97alim@gmail.com>
2024-09-12 20:28:50 +0530

    👉 Map Utilities function added for User and driver.

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,36 @@
+import { calculateRegion } from "@/lib/map";
+import { useLocationStore } from "@/store";
 import { Text, View } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 const Map = () => {
+  const {
+    userLatitude,
+    userLongitude,
+    destinationLatitude,
+    destinationLongitude,
+  } = useLocationStore();
+
+  const region = calculateRegion({
+    userLatitude,
+    userLongitude,
+    destinationLatitude,
+    destinationLongitude,
+  });
+
   return (
-    <View>
-      <Text>Your Map</Text>
-    </View>
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutatedStandard"
+      showsPointsOfInterest={false}
+      initialRegion={region}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
+      <Text>Google Maps</Text>
+    </MapView>
   );
 };
 
