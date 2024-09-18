97067c6e66264f8a48764b03f7b83163d504a7bd
arafat-alim <arafat97alim@gmail.com>
2024-09-19 00:37:01 +0530

    ✨ Driver fetch api is created.

---
+++ app/(api)/driver+api.ts
@@ -0,0 +1,27 @@
+import { neon } from "@neondatabase/serverless";
+
+export async function GET() {
+  try {
+    const sql = neon(process.env.DATABASE_URL!);
+    const response = await sql(`SELECT * from drivers`);
+
+    return Response.json(
+      {
+        data: response,
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.log("Error occured while fetching drivers data", error);
+    return Response.json(
+      {
+        error: "Internal server error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
