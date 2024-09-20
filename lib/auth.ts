d2c3b69fc882ea76025b6a86cb87351707cb55e9
arafat-alim <arafat97alim@gmail.com>
2024-09-20 20:40:46 +0530

    ✨ OAuth API created.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import * as SecureStore from "expo-secure-store";
-
+import * as Linking from "expo-linking";
+import { fetchApi } from "./fetch";
 // export interface TokenCache {
 //   getToken: (key: string) => Promise<string | undefined | null>;
 //   saveToken: (key: string, token: string) => Promise<void>;
@@ -29,4 +30,55 @@ export const tokenCache = {
       return;
     }
   },
+};
+
+export const googleOAuth = async (startOAuthFlow: any) => {
+  try {
+    const { createdSessionId, signUp, setActive } = await startOAuthFlow({
+      redirectUrl: Linking.createURL("/(root)/(tabs)/home", {
+        scheme: "myapp",
+      }),
+    });
+
+    if (createdSessionId) {
+      //! If already logged in before then just authenticate and redirect the user to the home screen
+      if (setActive) {
+        setActive!({ session: createdSessionId });
+
+        //! If new user found then we create a new account on the clerk and neon database then redirect the user to the home screen
+
+        if (signUp.createdUserId) {
+          await fetchApi("/(api)/user", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              name: `${signUp.firstName} ${signUp.lastName}`,
+              email: `${signUp.emailAddress}`,
+              clerkId: `${signUp.createdUserId}`,
+            }),
+          });
+        }
+
+        return {
+          success: true,
+          code: "success",
+          message: "You have successful signin with Google",
+        };
+      }
+    }
+    return {
+      success: false,
+      code: "success",
+      message: "An error occurred while signing in with Google",
+    };
+  } catch (error: any) {
+    console.log("Error occured in GoogleOAuth Flow", error);
+    return {
+      success: false,
+      code: error.code,
+      message: error?.errors[0]?.longMessage,
+    };
+  }
 };
